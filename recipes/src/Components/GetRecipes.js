f4fc4e248147af8b5e63cc7a47f0274588660e75
Rachel Eldridge <racheleldridge99@gmail.com>
2023-12-29 05:30:28 +0000

    attempting the API swap
    Not quite got it working but I'm getting the layout right

--- recipes/src/Components/GetRecipes.js
+++ recipes/src/Components/GetRecipes.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React, {useState, useEffect} from 'react';
-// import { Container} from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 //https://www.themealdb.com/api.php
@@ -15,16 +15,17 @@ import '../App.css';
     //"strIngredient 1-20 - Ingredients 
     //"strMeasure 1-20 - Measurments for those ingredients
 const GetRecipes = () => {
+    const {isAuthenticated } = useAuth0();
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [users, setUsers] = useState([]);
+    const [meals, setMeals] = useState([]);
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users/")
+        fetch("http://www.themealdb.com/api/json/v1/1/random.php")
             .then(res => res.json())
             .then(
                 (data) => {
                     setIsLoaded(true);
-                    setUsers(data);
+                    setMeals(data);
                 },
                 (error) => {
                     setIsLoaded(true);
@@ -32,21 +33,43 @@ const GetRecipes = () => {
                 }
             )
         }, [])
-        if (error) {
-            return <div>Error: {error.message}</div>;
+        if (error && isAuthenticated) {
+            return <Container><div class="col-lg-6 mx-auto"><p class="lead mb-4">Error: {error.message}</p></div></Container>;
         } 
-        else if (!isLoaded) {
-            return <div>Loading...</div>;
+        else if (!isLoaded&& isAuthenticated) {
+            return <Container><div class="col-lg-6 mx-auto"><p class="lead mb-4">Loading...</p></div></Container>;
         }
-        else {
+        else if (isAuthenticated) {  
             return (
-            <ul>
-                {users.map(user => (
-                <li key={user.id}>
-                    {user.name} 
-                </li>
-                ))}
-            </ul>
+                <Container>
+                    <div class="row justify-content-md-center">
+                        <div class="col-md-6 padding">
+                            <div class="h-100 p-5 text-white bg-light rounded-3 lightestcard">
+                                {meals.map(meal => (<h2>{meal.strMeal}</h2>))}
+                                <img src={meals.map(meal => (<h2>{meal.strMealThumb}</h2>))} alt="meals.strMeal"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-md-stretch">
+                        <div class="col-md-9 padding" >
+                            <div class="h-100 p-5 text-white bg-light rounded-3 lightestcard">
+                                <h2>Instructions</h2>
+                                <p>{meals.strInstructions}</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 padding" >
+                            <div class="h-100 p-5 text-white bg-light rounded-3 lightestcard">
+                                <h2>Ingredients</h2>
+                                <ul>
+                                    <li>Item 1 - Amount 1</li>
+                                    <li>Item 2 - Amount 2</li>
+                                    <li>Item 3 - Amount 3</li>
+                                    <li>Item 4 - Amount 4</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+            </Container>
         );
     }
 }
